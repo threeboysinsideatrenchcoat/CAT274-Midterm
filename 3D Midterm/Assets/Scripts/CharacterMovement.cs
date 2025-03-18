19e55f3a7b2fe807e33b7d730f2085f0ea380514
threeboysinsideatrenchcoat <157040871+threeboysinsideatrenchcoat@users.noreply.github.com>
2025-03-18 17:48:01 -0400

    beep beep

--- 3D Midterm/Assets/Scripts/CharacterMovement.cs
+++ 3D Midterm/Assets/Scripts/CharacterMovement.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharacterMovement : MonoBehaviour
 {
     Rigidbody rb;
     public float speed = 0.5f;
+    public GameObject player;
+
+    public float lookSpeed = 5f;
+    public GameObject myCam;
+    public float camLock = 90;
+
+    public int CarrotCollect = 0;
+
+    Vector3 myLook;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        myLook = Vector3.zero;
     }
 
     // Update is called once per frame
@@ -34,7 +46,66 @@ public class CharacterMovement : MonoBehaviour
         {
             rb.AddForce(Vector3.right * speed);
         }
+        //What happens when player collects carrots 
+
+        if (CarrotCollect == 5)
+        {
+            SceneManager.LoadScene("Good Job");
+            Destroy(gameObject);
+        }
+
+        //if (CarrotCollect <= 4)
+        //{
+            ///SceneManager.LoadScene("Try Again");
+            //Destroy(gameObject);
+        //}
+
+        //Camera 
+
+        Vector3 playerLook = myCam.transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position, playerLook * 3f, Color.magenta);
+
+        myLook += DeltaLook() * Time.deltaTime;
+        if (myLook.y > camLock)
+        {
+            myLook.y = camLock;
+        }
+        else if (myLook.y < -camLock)
+        {
+            myLook.y = -camLock;
+        }
+
+        transform.rotation = Quaternion.Euler(0f, myLook.x, 0f);
+        myCam.transform.rotation = Quaternion.Euler(-myLook.y, myLook.x, 0f);
+
     }
+    void FixedUpdate()
+    {
+        Vector3 myDir = transform.TransformDirection(Dir());
+        Vector3 Dir()
+        {
+
+            Vector3 moveDir = Vector3.zero;
+
+            float x = Input.GetAxisRaw("Horizontal");
+            float z = Input.GetAxisRaw("Vertical");
+            moveDir = new Vector3(x, 0, z);
+            return moveDir;
+        }
+
+        
+    }
+
+    Vector3 DeltaLook()
+    {
+        Vector3 deltaLook = Vector3.zero;
+        float rotY = Input.GetAxisRaw("Mouse Y") * lookSpeed;
+        float rotX = Input.GetAxisRaw("Mouse X") * lookSpeed;
+
+        deltaLook = new Vector3(rotX, rotY, 0);
+        return deltaLook;
+    }
+
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "Carrot")
@@ -42,5 +113,4 @@ public class CharacterMovement : MonoBehaviour
             Destroy(collision.gameObject);
         }
     }
-
 }
